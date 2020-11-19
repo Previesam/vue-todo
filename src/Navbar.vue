1ca6d2196c203d68450adc76ec1a7e845b4371fe
Previesam <samsmart.ass59@gmail.com>
2020-11-19 10:58:42 +0100

    Added search bar to the app bar, added table and buttons to the todolist page

--- src/Navbar.vue
+++ src/Navbar.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <v-app-bar-nav-icon left color="grey--text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-app-bar app color="primary" dark dense>
+      <v-app-bar-nav-icon
+        left
+        color="grey--text"
+        @click.stop="drawer = !drawer"
+      ></v-app-bar-nav-icon>
       <div class="d-flex align-center">
         <!--<v-img-->
         <!--  alt="Vuetify Logo"-->
@@ -22,19 +22,39 @@
       </div>
 
       <v-spacer></v-spacer>
+      <v-col class="col-4">
+        <v-text-field
+          outlined
+          prepend-inner-icon="search"
+          placeholder="Search"
+          dense="8"
+          class="mt-6 mr-4"
+          max-width="250px"
+          color="accent"
+        ></v-text-field>
+      </v-col>
 
-      <v-btn
-        to="/about"
-        text
-        outlined
-      >
+      <v-btn to="/about" text outlined>
         <span>Login</span>
         <v-icon right>dashboard</v-icon>
       </v-btn>
     </v-app-bar>
-    <v-navigation-drawer class="sidenav" dark app v-model="drawer" color="primary" width="256">
+    <v-navigation-drawer
+      class="sidenav"
+      dark
+      app
+      v-model="drawer"
+      color="primary"
+      width="256"
+    >
       <v-list>
-        <v-list-item v-for="(link, i) in links" :key="i" router :to="link.url" class="accent--text text-uppercase">
+        <v-list-item
+          v-for="(link, i) in links"
+          :key="i"
+          router
+          :to="link.url"
+          class="accent--text text-uppercase"
+        >
           <v-icon left>{{ link.icon }}</v-icon>
           <span class="ml-2">{{ link.name }}</span>
         </v-list-item>
@@ -44,13 +64,13 @@
 </template>
 
 <script>
-  export default {
-    name: "Navbar",
-    data() {
-      return {
-        drawer: this.$store.state.drawer,
-        links: this.$store.state.links
-      }
-    }
-  }
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      drawer: this.$store.state.drawer,
+      links: this.$store.state.links,
+    };
+  },
+};
 </script>
--- src/views/MyTodo.vue
+++ src/views/MyTodo.vue
@@ -1,14 +1,39 @@
 <template>
   <div class="my-todo ma-4">
-     <div class="title accent--text"
-      ><em>{{ this.$route.name }}</em></div
-    >
-    
-    
+    <v-row>
+      <v-col class="col-auto">
+        <div class="title accent--text">
+          <em>{{ this.$route.name }}</em>
+        </div>
+      </v-col>
+      <v-spacer></v-spacer>
+       <v-col class="col-auto">
+        <div>
+          <v-btn text outlined color="primary">
+            <v-icon small left>add</v-icon>
+            <span class="text-capitalize small">New</span>
+          </v-btn>
+        </div>
+      </v-col>
+      <v-col class="col-auto mr-10">
+        <div>
+          <v-btn text outlined color="primary">
+            <v-icon small left>input</v-icon>
+            <span class="text-capitalize small">Import</span>
+          </v-btn>
+        </div>
+      </v-col>
+    </v-row>
+
     <v-container fluid>
       <v-row>
         <v-col>
-          
+          <v-data-table
+            :headers="headers"
+            :items="todos"
+            :items-per-page="5"
+            class="elevation-1"
+          ></v-data-table>
         </v-col>
       </v-row>
     </v-container>
@@ -16,13 +41,38 @@
 </template>
 
 <script>
-  export default {
-    name: "My Todo",
-    data () {
-      
-      return {
-        
-      }
-    }
-  }
+export default {
+  name: "My Todo",
+  data() {
+    return {
+      headers: [
+        {
+          text: "Description",
+          align: "start",
+          sortable: true,
+          value: "description",
+        },
+        {
+          text: "Status",
+          align: "start",
+          sortable: true,
+          value: "status",
+        },
+        {
+          text: "User",
+          align: "start",
+          sortable: true,
+          value: "user",
+        },
+        {
+          text: "Type",
+          align: "start",
+          sortable: true,
+          value: "type",
+        },
+      ],
+      todos: this.$store.state.todos,
+    };
+  },
+};
 </script>
